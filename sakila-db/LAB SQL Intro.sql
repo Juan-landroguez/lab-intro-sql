0655351e2e1896db6197de5d745767260828f889
juan Landroguez <juan.landroguez@gmail.com>
2023-02-28 17:27:33 +0100

    SEQUEL

---
+++ sakila-db/LAB SQL Intro.sql
@@ -0,0 +1,19 @@
+USE sakila;
+
+SELECT * FROM actor;
+
+SELECT * FROM film;
+
+SELECT * FROM customer;
+
+SELECT title  FROM film;
+
+SELECT distinct title FROM film;
+
+SELECT distinct name FROM language;
+
+SELECT count(store_id) FROM store;
+
+SELECT count(staff_id) FROM staff;
+
+SELECT first_name FROM staff;
